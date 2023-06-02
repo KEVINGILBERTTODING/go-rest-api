5d000cd20e6071b27c61ded4404ddbc7dded2cac
KEVINGILBERTTODING <122202002832@mhs.dinus.ac.id>
2023-06-03 04:16:29 +0700

    my first REST API using Go

---
+++ models/setup.go
@@ -0,0 +1,18 @@
+package models
+
+import (
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func ConnectDatabase() {
+	database, err := gorm.Open(mysql.Open("root:@tcp(localhost:3306)/toko_baju"))
+	if err != nil {
+		panic(err)
+	}
+	database.AutoMigrate(&Product{})
+
+	DB = database
+}
